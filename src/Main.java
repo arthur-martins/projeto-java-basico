7dd3493665f351c0ff9cf312feff3b33c448e933
arthur-martins <arthurm.dev@gmail.com>
2024-12-04 20:39:44 -0300

    Adiciona verificação de maioridade

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,14 @@ public class Main {
         System.out.println("Digite seu nome: ");
         String nome = scanner.nextLine();
         System.out.println("Olá, " + nome + "!");
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade < 18) {
+            System.out.println("Você é menor de idade!");
+        } else {
+            System.out.println("Você é maior de idade!");
+        }
     }
 }
